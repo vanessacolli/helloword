eb19c7f5a06882c8ac7e3de92a97740100fec8a1
vanessacolli <vcolli.2011@gmail.com>
2024-10-15 11:50:06 -0300

    Create app01.py
    Aula dia15 de outubro

---
+++ app01.py
@@ -0,0 +1,30 @@
+#Criar uma variável
+'''Esse 
+comentário
+pode
+conter
+muitas
+linhas
+'''
+
+#Cria uma variável usando "snake_case"
+cor_da_casa = "azul"
+
+#Crie uma variável usando"camelcase"
+corDaCasa = "amarelo"
+
+#Exibir as coisas
+print('\n\n\n')
+print(cor_da_casa)
+print(corDaCasa)
+
+#primeiro nome
+first_name = "Joca"
+#Último nome
+last_name = "Silva"
+
+#Imprima seu nome
+print(first_name)
+print(last_name)
+#Imprime os dois nomes
+print(first_name, last_name)
